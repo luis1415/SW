24c4110ff563ae8c31461f966a5d044498fad190
luis1415 <luisbermudeztorres@gmail.com>
2016-09-24 14:42:42 -0500

    edit album

--- views/home.php
+++ views/home.php
@@ -13,7 +13,7 @@
 
     <!-- Bootstrap Core CSS -->
     <link href="../stylesheets/css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="../stylesheets/css/signin.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="../stylesheets/css/thumbnail-gallery.css" rel="stylesheet">
 
@@ -22,7 +22,7 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <link href="../stylesheets/css/signin.css" rel="stylesheet">
+
     <![endif]-->
 
 </head>
